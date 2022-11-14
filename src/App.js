25bb41d78fc6567e64379e837c1479d0e9cb5c9f
Arpit <arpitpandeyofc@gmail.com>
2022-11-15 01:03:54 +0530

    Bug fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from '../src/pages/homepage/Homepage';
 import Brandlogin from '../src/pages/brandlogin/Brandlogin';
 import Brandsignup from '../src/pages/brandsignup/BrandSignup';
