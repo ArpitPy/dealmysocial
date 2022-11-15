bbdc15c39b197249259d0ac02d7ea2b33a661faf
Arpit <arpitpandeyofc@gmail.com>
2022-11-15 09:15:53 +0530

    Logo fixed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import Logo from "../../assets/logo.png";
 import "./topbar.css";
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="Navbar">
-        <img className="logo" src={Logo} alt="" />
+      <Link className={"logo"} to="/"><img src={Logo} alt="" /></Link>
       <div className={`nav-items ${isOpen && "open"}`}>
-        <a href="/">Home</a>
+        <a href="/">About Us</a>
         <a href="/about">Community</a>
         <a href="/loginer">Log In</a>
         <a href="/signer">Sign Up</a>
